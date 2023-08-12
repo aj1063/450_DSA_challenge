786a770af63261140a44900a93c2574e52555cd0
aj1063 <60145398+aj1063@users.noreply.github.com>
2023-08-13 03:53:48 +0530

    Update Overlapping Intervals.cpp

--- Overlapping Intervals.cpp
+++ Overlapping Intervals.cpp
@@ -31,30 +31,26 @@ public:
       return ans;   
     }
 };
+///////////////////////SECOND Approach
 
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<vector<int>>Intervals(n);
-		for(int i = 0; i < n; i++){
-			int x, y;
-			cin >> x >> y;
-			Intervals[i].push_back(x);
-			Intervals[i].push_back(y);
-		}
-		Solution obj;
-		vector<vector<int>> ans = obj.overlappedInterval(Intervals);
-		for(auto i: ans){
-			for(auto j: i){
-				cout << j << " ";
-			}
-		}
-		cout << "\n";
-	}
-	return 0;
+int n = intervals.size();
+         sort(intervals.begin(), intervals.end());
+    //      //sort(intervals.begin(), intervals.end(), sort_pair_first<int, int>());
+         vector<vector<int>>ans;
+         vector<int>v = intervals[0];
+         int i = 1;
+     while(i < n) {
+         
+         
+         if(intervals[i][0] <= v[1])    {
+             v[1] = max(v[1], intervals[i][1]);
+         } else {
+             ans.push_back(v);
+             v = intervals[i];
+         }
+         i++;
+     }
+     ans.push_back(v);
+
+return ans;
 }
-// }
