0785689da987a8b8beb7a49580af364b2da6a026
aj1063 <60145398+aj1063@users.noreply.github.com>
2023-10-10 20:31:08 +0530

    Create count (n devide k) .cpp

---
+++ count (n devide k) .cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+    //Function to find all elements in array that appear more than n/k times.
+    int countOccurence(int arr[], int n, int k) {
+        // Your code here
+        unordered_map<int, int>freq;
+        int x = (n / k);
+        for(int i = 0; i < n; i++){
+            freq[arr[i]]++;
+        }
+        int c = 0;
+        for(auto i : freq){
+            if(i.second > x)c++;
+        }
+        return c;
+        
+    }
+};
