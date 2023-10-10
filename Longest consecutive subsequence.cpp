e080006b610f541e9a74e705e89fd707c8e30f42
aj1063 <60145398+aj1063@users.noreply.github.com>
2023-10-10 20:26:02 +0530

    Update Longest consecutive subsequence.cpp

--- Longest consecutive subsequence.cpp
+++ Longest consecutive subsequence.cpp
@@ -25,3 +25,33 @@ class Solution{
      return ans; 
      
     }
+
+
+
+
+Longest consecutive subsequence
+class Solution{
+  public:
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+    int findLongestConseqSubseq(int arr[], int N)
+    {
+      //Your code here
+      sort(arr,arr+N);
+      vector<int>v;
+      v.push_back(arr[0]);
+      for(int i=1;i<N;i++){
+          if(v[i-1]!=arr[i])v.push_back(arr[i]);
+      }
+      int ans = 0;
+      int c = 0;
+      for(int i=0;i<v.size();i++){
+          if(i>0 && v[i] == (v[i-1]+1))c++;
+          else c=1;
+          ans = max(ans,c);
+      }
+      return ans;
+    }
+};
